5057ce92bac61148c9dc5eb2d77a919ab592b7a1
Filipe Gomes <filipe.gt1998@outlook.com>
2022-04-15 18:54:38 -0300

    This commit adds a new version to blog

--- src/components/molecules/Card/styles.ts
+++ src/components/molecules/Card/styles.ts
@@ -26,7 +26,7 @@ export const CardTop = styled.div`
     width: 100%;
     max-height: 300px;
     height: 200px;
-    /* object-fit: cover; */
+    object-fit: cover;
     cursor: pointer;
   }
 
@@ -104,7 +104,7 @@ export const ContainerTop = styled.div`
   img {
     width: 100%;
     height: 580px;
-    /* object-fit: cover; */
+    object-fit: cover;
     cursor: pointer;
   }
 
