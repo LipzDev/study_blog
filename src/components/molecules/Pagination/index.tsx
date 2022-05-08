2b5b9a9a1d48b7a8c3898ab43882343ef5ed13f7
Filipe Gomes <filipe.gt1998@outlook.com>
2022-05-08 00:14:06 -0300

    Adicionando parte do sistema de paginação

--- src/components/molecules/Pagination/index.tsx
+++ src/components/molecules/Pagination/index.tsx
@@ -6,33 +6,10 @@ const Pagination = ({ setShowContent, showContent }: any) => {
   return (
     <S.Container>
       <S.Pagination>
-<<<<<<< Updated upstream
-        <div className="prev">
-          <span onClick={() => setSelectedElement(selectedElement - 1)}>
-            Anterior
-          </span>
-        </div>
-        <S.Buttons>
-          {pages.map((page, index) => (
-            <button
-              key={index}
-              className={index === selectedElement ? "actived" : ""}
-              onClick={() => setSelectedElement(index)}
-            >
-              {page}
-            </button>
-          ))}
-        </S.Buttons>
-        <div className="next">
-          <span onClick={() => setSelectedElement(selectedElement + 1)}>
-            Próxima
-          </span>{" "}
-=======
         <div className="next">
           <span onClick={() => setShowContent(showContent + 1)}>
             Carregar mais
           </span>
->>>>>>> Stashed changes
         </div>
       </S.Pagination>
     </S.Container>
